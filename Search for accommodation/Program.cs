44017d9341ba6cdbded5b9b076388c742f2807aa
ankon <ankon@DESKTOP-703PMUF>
2021-09-11 13:29:35 +0300

    Добавьте файлы проекта.

---
+++ Search for accommodation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Search_for_accommodation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
